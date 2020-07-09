f59247476f877b42c7544f147dabb8f0d75ca63e
sheela <sheelavivin@gmail.com>
2020-07-09 12:54:00 -0400

    Testing GIT

---
+++ src/com/Training/Day12/CollectionOneG.java
@@ -0,0 +1,122 @@
+package com.Training.Day12;
+
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CollectionOneG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//old styLE
+		
+	//	List students = new ArrayList ();
+		
+		//NeW style of using collections After GENERICS
+		List<String> students = new ArrayList<String> ();
+	//	ArrayList students = new ArrayList ();	
+		
+		//Java Recommends interface driven variable creation and reference
+		//I.e If yoU CREATE an object< suggest to refer it with a base interface type
+		//i CAN oNLY PUT InTEGERS IN THE coLLECTIon object
+		
+		
+		//adding to colLection
+		students.add("mahesh");  //0  
+		students.add("Shaaz");  //1
+		students.add("mahesh");  //2
+		students.add("dipak");  //3
+		students.add("Garry");  //4
+		
+		//PRInt coLLECTiOn
+		System.out.println(students);
+		
+		
+		//READ an element FROM CoLLECTioN
+		
+  //   System.out.println(students[2]); arraY STYle not POSSIBLE
+	  System.out.println(students.get(3)); 
+	  
+	  //removing from collection
+	  
+	  students.remove(3);
+	  
+	  System.out.println(students);  //NOTIce thaT THE PosItIonS aRE AdJUSTED
+	  
+		students.add("dipak");  // ALWAYs done at ThE end
+		 System.out.println(students);
+		 
+	 	 
+		 students.remove("mahesh") ;  //FIRST OCCurAnCe gets DeleTeD As duplicatES preseNT
+		 
+		 System.out.println(students);
+		 students.add("mahesh");
+		 
+		boolean b= students.contains("mahesh");
+		System.out.println(b);
+	
+		boolean c= students.contains("maheshRR");
+		System.out.println(c);
+		
+		System.out.println(students.contains("mahesh"));
+		
+		if(students.contains("Trump") )
+			System.out.println("i GOT A MATCH");
+		else
+			System.out.println("oops");
+		
+		
+		
+		students.clear();
+		System.out.println(students);
+		
+		
+		students.add("mahesh");  //0  
+		students.add("Shaaz");  //1
+		students.add("mahesh");  //2
+		students.add("dipak");  //3
+		students.add("Garry");  //4
+		
+		System.out.println(students);
+		
+		students.add(2, "sheela");  //now arrayliST SIzE gEts INCrEAseD
+		
+		
+		System.out.println(students);
+		
+		int indexOfB= students.indexOf("dipak");
+		System.out.println(indexOfB);
+		
+		Iterator<String> itr = students.iterator();
+		
+		
+		//styLE PRIOR To JAVA 5
+		while(itr.hasNext())
+			System.out.println(itr.next());
+		
+		System.out.println("---------------");
+		
+		//NEW TYPE oF FOR LOop SINCE JAVA 5
+		for (Object object : students) {   
+			System.out.println(object);
+		}
+
+		//thIS is similr to
+		
+		while(itr.hasNext()) {
+		 	Object name1=  itr.next();  //No caSTiNG nEEdeD
+			String name= itr.next();  //explicit CaSting needed
+			System.out.println(name);
+		}
+		
+		System.out.println("---------------");
+		
+		
+			
+		
+	}
+
+}
